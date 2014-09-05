33e30281cb859e99a5cbef5a8080b66e9a57d075
Sophia Davis <scdgrapefruit@gmail.com>
2014-09-05 13:29:17 -0400

    Add contact

--- db.py
+++ db.py
@@ -4,13 +4,13 @@ conn = sqlite3.connect('phonebook.db', check_same_thread= False)
 
 def create_db():
     c = conn.cursor()
-    c.execute('CREATE TABLE phonebook ( id integer AUTOINCREMENT phonebook_name text name text phonenum text unique)')
+    c.execute('CREATE TABLE phonebook ( id INTEGER PRIMARY KEY AUTOINCREMENT, phonebook_name TEXT, name TEXT, phonenum TEXT UNIQUE)')
     conn.commit()
 
 def add_entry(phonebook, entry):
     try:
         with conn:
-            conn.execute('INSERT INTO phonebook VALUES (?, ?, ?)', phonebook, entry)
+            conn.execute('INSERT INTO phonebook VALUES (NULL, ?, ?, ?)', (phonebook, entry[0], entry[1]))
         return True
     except sqlite3.IntegrityError:
         return False
--- phonebook.py
+++ phonebook.py
@@ -1,20 +1,51 @@
-def create():
-    pass
-    
-def lookup():
-    pass
+import sqlite3
+import sys
 
-def add():
-    pass
+import db
+
+def create(phonebook):
+    try:
+        db.create_db()
+    except sqlite3.OperationalError:
+        print "Phonebook exists"
+
+def add(name, number, phonebook):
+    status = db.add_entry(phonebook, (name, number))
+    if status:
+        print "%s added to %s with number %s" % (name, phonebook, number)
+    else:
+        print "Error: phonenumber %s already present in %s" % (number, phonebook)
 
 def change():
     pass
+    
+def lookup(name, phonebook):
+    results = db.lookup(name, phonebook)
+    print results
 
+def reverse_lookup(number, phonebook):
+    results = db.lookup(name, phonebook)
+    print results
+    
 def remove():
     pass
 
-def reverse-lookup():
-    pass
+command_dict = {"create" : create,
+                "add" : add,
+                "change" : change,
+                "lookup" : lookup,
+                "reverse-lookup" : reverse_lookup,
+                "remove" : remove}
 
 if __name__ == "__main__":
-    pass
+    args = sys.argv
+    args = args[1:]
+    command = args.pop(0)
+    
+    try:
+        to_run = command_dict[command]
+    except KeyError: 
+        sys.exit("No such command")
+    
+    to_run(*args)    
+

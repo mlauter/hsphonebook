8396f1b697db0b5e8ffa912de05588f964a3f931
mlauter <lauter.miriam@gmail.com>
2014-09-05 13:05:43 -0400

    support multiple phonebooks

--- db.py
+++ db.py
@@ -4,26 +4,26 @@ conn = sqlite3.connect('phonebook.db', check_same_thread= False)
 
 def create_db():
     c = conn.cursor()
-    c.execute('CREATE TABLE phonebook ( id integer AUTOINCREMENT name text phonenum text unique)')
+    c.execute('CREATE TABLE phonebook ( id integer AUTOINCREMENT phonebook_name text name text phonenum text unique)')
     conn.commit()
 
-def add_entry(entry):
+def add_entry(phonebook, entry):
     try:
         with conn:
-            conn.execute('INSERT INTO phonebook VALUES (?, ?)', entry)
+            conn.execute('INSERT INTO phonebook VALUES (?, ?, ?)', phonebook, entry)
         return True
     except sqlite3.IntegrityError:
         return False
 
-def lookup(name):
+def lookup(phonebook, name):
     c = conn.cursor()
-    c.execute('SELECT * FROM phonebook WHERE name=?', name)
+    c.execute('SELECT * FROM phonebook WHERE phonebook=? AND name=?', phonebook, name)
     data = c.fetchall()
     return data
 
-def reverse_lookup(number):
+def reverse_lookup(phonebook, number):
     c = conn.cursor()
-    c.execute('SELECT * FROM phonebook WHERE phonenum=?', number)
+    c.execute('SELECT * FROM phonebook WHERE phonebook=? AND phonenum=?', phonebook, number)
     data = c.fetchall()
     return data
 

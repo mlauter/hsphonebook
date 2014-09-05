d3d4e2d20cc67659a12f93b0d0ffe08ab6d7cbf6
Sophia Davis <scdgrapefruit@gmail.com>
2014-09-05 12:24:18 -0400

    Add code skeleton

---
+++ phonebook.py
@@ -0,0 +1,20 @@
+def create():
+    pass
+    
+def lookup():
+    pass
+
+def add():
+    pass
+
+def change():
+    pass
+
+def remove():
+    pass
+
+def reverse-lookup():
+    pass
+
+if __name__ == "__main__":
+    pass
